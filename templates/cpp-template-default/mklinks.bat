15af0150d1be6ab45d103e6bb91bb436524cca3b
Neurochrom <pcichock@poczta.onet.pl>
2015-10-22 17:37:34 +0200

    Updated start instructions, and Android build scripts, and added a template .gitignore and a link creating .bat.

---
+++ templates/cpp-template-default/mklinks.bat
@@ -0,0 +1,3 @@
+mklink /J cocos2d ..\cocos2d-x
+rem mklink /J openal-soft-winphone ..\openal-soft-winphone
+rem mklink /J openal-soft-android ..\openal-soft-android
--- templates/cpp-template-default/proj.android/omega-release.sh
+++ templates/cpp-template-default/proj.android/omega-release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 py build_lib.py -b release
 ant clean release -f build.xml -Dsdk.dir=$ANDROID_SDK_ROOT
-adb -d install -r bin/fsm-release.apk
+adb -d install -r bin/YOUR_APP_NAME_HERE-release.apk
 adb -d logcat -c
 adb -d logcat -s prortus cocos2d-x
